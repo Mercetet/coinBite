16f743bed71914f64b5bb2bf391fbf5543678366
Merce <mercetetilla@gmail.com>
2025-09-02 21:28:57 -0300

    Primer commit de CoinBite

--- script.js
+++ script.js
@@ -1,39 +1,61 @@
-function calcular(){
-    const sueldo = parseFloat(document.getElementById("sueldo").value);
-    const dias = parseFloat(document.getElementById("dias").value);
-    const horas = parseFloat(document.getElementById("horas").value);
-    const producto = parseFloat(document.getElementById("producto").value);
+function calcular() {
+  const sueldo = parseFloat(document.getElementById("sueldo").value);
+  const dias = parseFloat(document.getElementById("dias").value);
+  const horas = parseFloat(document.getElementById("horas").value);
+  const producto = parseFloat(document.getElementById("producto").value);
+  const repeticion = parseFloat(document.getElementById("repeticion").value);
 
-    if (!sueldo || !dias || !horas || !producto) {
-        alert("Por favor, ingresa valores numéricos válidos en todos los campos.");
-        return;
-    }
+  if (!sueldo || !dias || !horas || !producto) {
+    alert("Por favor, ingresa valores numéricos válidos en todos los campos.");
+    return;
+  }
 
-    if (sueldo <= 0 || dias <= 0 || horas <= 0 || producto <= 0) {
-        alert("Por favor, ingresa valores mayores que cero en todos los campos.");
-        return;
-    }
+  if (
+    sueldo <= 0 ||
+    dias <= 0 ||
+    horas <= 0 ||
+    producto <= 0 ||
+    repeticion <= 0
+  ) {
+    alert("Por favor, ingresa valores mayores que cero en todos los campos.");
+    return;
+  }
 
-    if (dias > 7) {
-        alert("Los días trabajados por semana no pueden ser más de 7.");
-        return;
-    }
+  if (dias > 7) {
+    alert("Los días trabajados por semana no pueden ser más de 7.");
+    return;
+  }
 
-    if (horas > 24) {
-        alert("Las horas trabajadas por día no pueden ser más de 24.");
-        return;
-    }
+  if (horas > 24) {
+    alert("Las horas trabajadas por día no pueden ser más de 24.");
+    return;
+  }
 
-    
-    const horasMes = horas * dias * 4.33;
-    const precioHora = sueldo / horasMes;
-    const tiempoNecesario = producto / precioHora;
+  if (!repeticion) {
+    repeticion = 1;
+  }
 
-    const diasNecesarios = Math.floor(tiempoNecesario / horas);
-    const horasRestantes =tiempoNecesario % horas.toFixed(2);
+  const horasMes = horas * dias * 4.33;
+  const precioHora = sueldo / horasMes;
+  const tiempoNecesario = (producto * repeticion) / precioHora;
 
-    const horasNecesarias = Math.floor(horasRestantes);
-    const minutosNecesarios = Math.round((horasRestantes - horasNecesarias) * 60);
+  const diasNecesarios = Math.floor(tiempoNecesario / horas);
+  const horasRestantes = tiempoNecesario % horas;
 
-    document.getElementById("total").innerText = `El producto vale al equivalente de ${diasNecesarios} días y ${horasNecesarias} horas con ${minutosNecesarios} minutos de trabajo.`;
+  const horasNecesarias = Math.floor(horasRestantes);
+  const minutosNecesarios = Math.round((horasRestantes - horasNecesarias) * 60);
+
+  let textoDia = "días";
+  if (diasNecesarios === 1) textoDia = "día";
+
+
+  let textoHora = "horas";
+  if (horasNecesarias === 1) textoHora = "hora";
+
+  let textoMinuto = "minutos";
+  if (minutosNecesarios === 1) textoMinuto = "minuto";
+
+  document.getElementById("total").innerText = `Tu "pequeño" gasto cuesta $${
+    producto * repeticion
+  }. Equivale a ${diasNecesarios} ${textoDia}, ${horasNecesarias} ${textoHora} y ${minutosNecesarios} ${textoMinuto} de tu trabajo.`;
 }
